6bc434df13321e58aa849483c8a820ccdf85b9ed
FernandinOficial <fernandoribeirooficial123@gmail.com>
2024-05-24 17:22:58 -0300

    primeiro commit

---
+++ index.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Studios Grimm</title>
+    <link rel="stylesheet" href="indexStyle.css">
+    <link rel="shortcut icon" href="Images/icon/LogoWhite_semfundo.png" type="image/x-icon">
+    <link rel="apple-touch-icon" href="favicon.png">
+    <link rel="stylesheet" href="Images/fonts/pixelFont/Pixel.ttf">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Pixelify+Sans:wght@400..700&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <script src="script.js"></script>
+    <header class="header">
+        <a href="">Notícias</a>
+        <a href="">Produtos</a>
+        <a href="">Suporte</a>
+        <a href="#sobre">Sobre</a>
+    </header>
+    <hr>
+    <div class="main">
+        <button class="hide-nav-button"><img class="hide-nav-button__img" src="Images/navIcon.png"
+                alt="Navegação 3 barras"></button>
+        <div class="dp__none">
+            <div class="nav__menu">
+                <ul class="nav">
+                    <a href="login.html" target="_blank"><img src="Images/loginIconBranco.png" alt="Perfil do Login"></a>
+                    <ul><a class="nav__a" href="">Produtos</a></ul>
+                    <ul><a class="nav__a" href="">Comunidade</a></ul>
+                    <ul><a class="nav__a" href="">Créditos</a></ul>
+                    <ul><a class="nav__sair" onclick="" href="">Sair</a></ul>
+                </ul>
+            </div>
+        </div>
+
+        <h1 class="texto__inicio"></h1>
+        <div class="pesquisar__menu">
+            <div class="pesquisar-container">
+                <input class="pesquisar" type="text" placeholder="Jogos / Produtos / Blog" maxlength="15">
+                <button type="button" class="source"><img class="source__img" src="Images/source.png"
+                        alt="Pesquisar"></button>
+            </div>
+        </div>
+
+        <svg style="background-color: #ffff;" width="100vw" height="70" viewBox="0 0 100 100"
+            preserveAspectRatio="none">
+            <path id="wavepath" d="M0,0  L110,0C35,150 35,0 0,100z" fill="#0e0e0e"></path>
+        </svg>
+
+    </div>
+    <br>
+    <br>
+    <br>
+    <div>
+        <section class="jogos">
+            <h1 class="jogos__texto">Jogos</h1>
+            <button class="button__jogos"><a href="EcosOfAbyss.html"><img class="img__jogos"
+                        src="Images/icon/ecosLogoImg2.png" alt="Ecos of Abyss"></a></button>
+            <button class="button__jogos"><a href="">
+                    <h1>Em Breve...</h1>
+                    </p>
+                </a></button>
+            <button class="button__jogos"><a href="">
+                    <h1>Em Breve...</h1>
+                </a></button>
+        </section>
+    </div>
+
+
+    <div>
+        <section class="noticias">
+            <h1 class="noticias__texto">Notícias</h1>
+            <button class="button__noticias"><a href="">
+                    <h1>Em Breve...</h1>
+                    </p>
+                </a></button>
+            <button class="button__noticias"><a href="">
+                    <h1>Em Breve...</h1>
+                    </p>
+                </a></button>
+            <button class="button__noticias"><a href="">
+                    <h1>Em Breve...</h1>
+                    </p>
+                </a></button>
+        </section>
+    </div>
+
+
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <div></div>
+    <svg style="background-color: #ffff;" width="100vw" height="70" viewBox="0 0 100 100" preserveAspectRatio="none"
+        transform="rotateX(180deg)">
+        <path id="wavepath" d="M0,0  L110,0C35,150 35,0 0,100z" fill="#0e0e0e"></path>
+    </svg>
+
+
+
+    <div class="footer">
+
+        <div id="sobre"></div>
+
+    </div>
+
+
+</body>
+
+</html>
---
+++ script.js
@@ -0,0 +1,157 @@
+/*document.addEventListener('DOMContentLoaded', function() {
+    // Verifica se o cookie de recarregamento já foi definido
+    const reloadCookie = document.cookie.replace(/(?:(?:^|.*;\s*)reload\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+
+    // Se o cookie de recarregamento não existir, recarrega a página e define o cookie
+    if (!reloadCookie) {
+        location.reload();
+        document.cookie = "reload=true; path=/"; // Define o cookie com uma validade de sessão
+    }
+});*/
+
+
+
+
+// Função para alterar o título
+function changeTitle() {
+    document.title = "Studios Grimm";
+}
+
+// Função para restaurar o título original
+function restoreTitle() {
+    document.title = "Volte aqui ;(";
+}
+
+// Adiciona um evento para quando a janela ganhar foco
+window.addEventListener('focus', changeTitle);
+
+// Adiciona um evento para quando a janela perder foco
+window.addEventListener('blur', restoreTitle);
+
+
+//recarrega a página
+window.addEventListener('focus', function() {
+    location.reload();
+});
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const phrases = [
+        "Explore o melhor dos jogos...",
+        "Descubra novas aventuras...",
+        "Conecte-se com outros jogadores  :)",
+        "Desbrave novas terras..."
+    ]; // Array contendo as frases para serem alternadas
+    let currentPhraseIndex = 0; // Índice da frase atual
+    const delay = 100; // Tempo de atraso entre cada caractere em milissegundos
+    let shuffledPhrases = shuffleArray(phrases.slice()); // Array de frases embaralhadas
+
+    // Função para embaralhar um array
+    function shuffleArray(array) {
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+        return array;
+    }
+
+    function typeWriter(text, i) {
+        if (i < text.length) {
+            document.querySelector('.texto__inicio').textContent += text.charAt(i);
+            i++;
+            setTimeout(() => {
+                typeWriter(text, i);
+            }, delay);
+        }
+    }
+
+    function changePhrase() {
+        // Limpa o conteúdo atual da '.texto__inicio'
+        document.querySelector('.texto__inicio').textContent = '';
+        // Verifica se todas as frases foram exibidas
+        if (currentPhraseIndex >= shuffledPhrases.length) {
+            // Embaralha as frases novamente
+            shuffledPhrases = shuffleArray(phrases.slice());
+            // Reinicia o índice da frase atual
+            currentPhraseIndex = 0;
+        }
+        // Obtém a próxima frase do array embaralhado
+        const nextPhrase = shuffledPhrases[currentPhraseIndex];
+        // Atualiza o índice para a próxima frase
+        currentPhraseIndex++;
+        // Inicia a animação da próxima frase
+        typeWriter(nextPhrase, 0);
+    }
+
+    // Inicia a animação com a primeira frase
+    typeWriter(shuffledPhrases[currentPhraseIndex], 0);
+
+    // Alterna entre as frases a cada 15 segundos
+    setInterval(changePhrase, 15000);
+});
+
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const navMenu = document.querySelector('.nav__menu');
+    const hideNavButton = document.querySelector('.hide-nav-button');
+
+    // Adiciona ouvinte de evento para clicar no botão de alternar menu
+    hideNavButton.addEventListener('click', function() {
+        navMenu.classList.toggle('hidden'); // Alterna a visibilidade do menu
+    });
+
+    // Adiciona ouvinte de evento para mouseout no menu de navegação
+    navMenu.addEventListener('mouseout', function(event) {
+        // Verifica se o evento de mouseout ocorreu fora do menu de navegação
+        if (!navMenu.contains(event.relatedTarget)) {
+            navMenu.classList.add('hidden'); // Oculta o menu se o mouse sair da área do menu
+        }
+    });
+});
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const navMenu = document.querySelector('.nav__menu');
+    const hideNavButton = document.querySelector('.hide-nav-button');
+
+    // Adiciona ouvinte de evento para clicar no botão de alternar menu
+    hideNavButton.addEventListener('click', function() {
+        // Altera o estilo do menu para vazio ou block
+        navMenu.style.display = navMenu.style.display === 'none' ? 'block' : 'none';
+    });
+}); 
+
+
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const hideNavButton = document.querySelector('.hide-nav-button');
+
+    setInterval(function() {
+        hideNavButton.style.transition = 'transform 1s ease'; // Adiciona uma transição suave de 1 segundo para a propriedade transform
+        hideNavButton.style.transform = 'rotate(360deg)'; // Aplica uma rotação de 360 graus
+        setTimeout(function() {
+            hideNavButton.style.transform = 'rotate(0deg)'; // Retorna ao estado original
+        }, 1000); // Após 1 segundo, retorna ao estado original
+    }, 20000); // Executa a animação a cada 20 segundos
+});
+
+
+
+if (!localStorage.getItem('reloaded')) {
+    // Marca a página como recarregada
+    localStorage.setItem('reloaded', 'true');
+    // Recarrega a página após 1 segundo
+    setTimeout(function() {
+        location.reload();
+    }, 1000);
+}
