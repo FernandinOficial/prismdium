26d5fa73bebe4171d154945c5e066d895815bf7b
FernandinOficial <fernandoribeirooficial123@gmail.com>
2024-06-06 22:57:13 -0300

    commit sql

--- sqlScripts/usuarios.sql
+++ sqlScripts/usuarios.sql
@@ -1,20 +1,5 @@
--- CREATE DATABASE prismdium;
 USE prismdium;
 
--- CREATE TABLE usuarios 
--- (
---     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
---     nome VARCHAR(50) NOT NULL,
---     usuario VARCHAR(50) NOT NULL UNIQUE, -- E-mail
---     senha VARCHAR(50) NOT NULL,
---     sexo ENUM('M', 'F', 'NS') NOT NULL, -- Inclui a opção 'Prefiro não dizer' = Not Say
---     foto_perfil BLOB -- Tipo de dado para armazenar imagens até 64KB
--- );
+INSERT INTO usuarios (nome, email, senha, sexo) VALUES ('teste', 'teste@gmail.com', '12345', 'NS');
 
-select * from usuarios;
-
--- INSERT INTO usuarios (id, nome, usuario, senha, sexo) VALUES (NULL, 'fernando', 'fernando@gmail.com', '12345', 'M');
-
-select * from usuarios;
-
--- ALTER TABLE usuarios CHANGE usuario email VARCHAR(255);
+SELECT * FROM usuarios;
